436eac77d4918bf28e02fbbebee3b5638de5910b
yangjunxin <yangjunxin@huya.com>
2025-04-17 20:32:57 +0800

    add control and container

---
+++ zedui/include/zedui/controls/container.h
@@ -0,0 +1,18 @@
+#ifndef ZEDUI_CONTROLS_CONTAINER_H_
+#define ZEDUI_CONTROLS_CONTAINER_H_
+
+#include <list>
+#include "zedui/controls/control.h"
+
+namespace zedui {
+class Container : public Control {
+ public:
+  void AddChild(std::shared_ptr<Control> child);
+  void RemoveChild(std::shared_ptr<Control> child);
+
+ private:
+  std::list<std::shared_ptr<Control>> childrens_;
+};
+}  // namespace zedui
+
+#endif  // ZEDUI_CONTROLS_CONTAINER_H_
