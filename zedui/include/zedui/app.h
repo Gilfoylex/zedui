616dbac56ec6b5f1d1b024170b06c2cff1e2046f
gilfoylex <gilfoylex@outlook.com>
2025-06-10 17:54:49 +0800

    add app

--- zedui/include/zedui/app.h
+++ zedui/include/zedui/app.h
@@ -2,21 +2,36 @@
 #define ZEDUI_APP_H_
 
 #include <memory>
+#include <queue>
 
 #include "zedbase/macros.h"
 #include "zedbase/thread.h"
+#include "zedbase/timer/timer.h"
+#include "zedbase/ui_task_runner.h"
+
 
 namespace zedui {
 class App {
  public:
+  static App* Current;
+
+ public:
   App();
   ~App();
+  std::shared_ptr<zedbase::UITaskRunner> GetUITaskRunner();
+
+ private:
+  void OnVsync();
 
  private:
   std::unique_ptr<zedbase::Thread> render_thread_;
+  std::shared_ptr<zedbase::UITaskRunner> ui_task_runner_;
+  std::unique_ptr<zedbase::ThreadTimer> vsync_timer_;
+  std::queue<zedbase::closure> vsync_tasks_;
 
   ZED_DISALLOW_COPY_ASSIGN_AND_MOVE(App);
 };
+
 }  // namespace zedui
 
 #endif  // ZEDUI_APP_H_
